fb40b2f672632b70078798ae8adec9a583d40fc0
ThanhSanglt02 <th.sang1210@gmail.com>
2025-10-02 22:27:16 +0700

    first comit

---
+++ booksale/products/models.py
@@ -0,0 +1,96 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models # models là tên của 1 package giống file reviews
+from django.contrib import auth 
+
+# Create your models here.
+class Publisher(models.Model):
+    """A company that publishes books."""
+    name = models.CharField(
+        max_length = 50,
+        help_text = "The name of the Publisher."
+    )
+    website = models.URLField(
+        help_text = "The Publisher's website."
+    )
+    email = models.EmailField(
+        help_text = "The Publisher's email address."
+    )
+
+    def __str__(self):
+        return self.name
+
+class Contributor(models.Model):
+    """A contributor to a Book, e.g. author, editor, coauthor."""
+    first_names = models.CharField(
+        max_length=50,
+        help_text="The contributor's first name or names.")
+    last_names = models.CharField(
+        max_length=50,
+        help_text="The contributor's last name or names.")
+    email = models.EmailField(
+        help_text="The contact email for the contributor.")
+    def initialled_name(self):
+        initials = ''.join([name[0] for name in self.first_names.split()])  # cắt chuỗi
+        return f"{self.last_names}, {initials}"
+
+    def __str__(self):
+        return self.initialled_name()
+    
+
+class Book(models.Model):
+    """A published book."""
+    title = models.CharField(
+        max_length=70,
+        help_text="The title of the book.")
+    publication_date = models.DateField(
+        verbose_name="Date the book was published.")
+    isbn = models.CharField(
+        max_length=20,
+        verbose_name="ISBN number of the book.")
+    publisher = models.ForeignKey(
+        Publisher,
+        on_delete=models.CASCADE)
+    contributors = models.ManyToManyField(
+        'Contributor',
+        through="BookContributor")
+    def __str__(self):
+        # return self.title
+        return "{} ({})".format(self.title, self.isbn)
+    
+class BookContributor(models.Model):
+    class ContributionRole(models.TextChoices):
+        AUTHOR = "AUTHOR", "Author"
+        CO_AUTHOR = "CO_AUTHOR", "Co-Author"
+        EDITOR = "EDITOR", "Editor"
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE)
+    contributor = models.ForeignKey(
+        Contributor,
+        on_delete=models.CASCADE)
+    # Thuộc tính role dùng dể lưu trữ thông tin bổ sung về mối quan hệ giữa lớp Book và Contributor. 
+    role = models.CharField(
+        verbose_name="The role this contributor had in the book.",
+        choices=ContributionRole.choices, max_length=20)
+    
+class Review(models.Model):
+    content = models.TextField(help_text="The Review text.")
+    rating = models.IntegerField(help_text="The rating the reviewer has given.")
+    date_created = models.DateTimeField(
+        auto_now_add=True,
+        help_text="The date and time the review was created.")
+    date_edited = models.DateTimeField(
+        null=True,
+        help_text="The date and time the review was last edited.")
+    # Thuộc tính creator liên kết đến model của bảng dữ liệu người
+    # dùng của Django ở dạng Một – Nhiều (phương thức auth.get_user_model() sẽ trả
+    # về model quản lý dữ liệu người dùng này).
+    creator = models.ForeignKey(
+        auth.get_user_model(),
+        on_delete=models.CASCADE)
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        help_text="The Book that this review is for.")
