f42f9619e2b41624bba9a40715d89e635a461536
ThanhSanglt02 <th.sang1210@gmail.com>
2025-10-22 22:48:35 +0700

    update commit

--- booksale/booksale_app/urls.py
+++ booksale/booksale_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, tonkho_list
 # from .views.home import HomeView
 
 urlpatterns = [
-    path('',home_view, name='home')
-
+    path('',home_view, name='home'),
+    path('stock/', tonkho_list, name = 'tonkho')
 ]
--- booksale/booksale_app/views/__init__.py
+++ booksale/booksale_app/views/__init__.py
@@ -1,5 +1,6 @@
 # Import các hàm view từ home.py
 from .home import index as home_view
+from .tonkho import createTonkho as tonkho_list
 
 # Import các hàm view từ product.py
 # from .product import index as product_index, detail as product_detail
