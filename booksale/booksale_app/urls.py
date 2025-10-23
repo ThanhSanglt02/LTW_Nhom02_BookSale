63a92d55c442a2b3bda28502d02e0d49547d16d8
hoangnguyentuyetdoan-eng <hoangnguyentuyetdoan@gmail.com>
2025-10-23 20:54:23 +0700

    first commit

--- booksale/booksale_app/urls.py
+++ booksale/booksale_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home_view, tonkho_list
+from .views import home_view, tonkho_list, product_detail
+
 # from .views.home import HomeView
 
 urlpatterns = [
     path('',home_view, name='home'),
-    path('stock/', tonkho_list, name = 'tonkho')
+    path('stock/', tonkho_list, name = 'tonkho'),
+    path('product_detail/', product_detail, name='product_detail')
 ]
