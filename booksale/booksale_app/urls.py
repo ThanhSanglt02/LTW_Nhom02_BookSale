dd451c23da1a7e02f352d707c634a0ddd8c23c75
dinhthuyvy1505-eng <dinhthuyvy1505@gmail.com>
2025-10-25 13:29:44 +0700

    vy commit

--- booksale/booksale_app/urls.py
+++ booksale/booksale_app/urls.py
@@ -1,10 +1,12 @@
+#urls.py
 from django.urls import path
-from .views import home_view, tonkho_list, product_detail
+from .views import home_view, tonkho_list, product_detail,giohang
 
 # from .views.home import HomeView
 
 urlpatterns = [
     path('',home_view, name='home'),
     path('stock/', tonkho_list, name = 'tonkho'),
-    path('product_detail/', product_detail, name='product_detail')
+    path('product_detail/', product_detail, name='product_detail'),
+    path('giohang/',giohang, name='giohang')
 ]
--- booksale/booksale_app/views/__init__.py
+++ booksale/booksale_app/views/__init__.py
@@ -2,5 +2,6 @@
 from .home import index as home_view
 from .tonkho import createTonkho as tonkho_list
 from .chitietsp import product_detail
+from .giohang import giohang
 # Import các hàm view từ product.py
 # from .product import index as product_index, detail as product_detail
