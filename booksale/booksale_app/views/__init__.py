e0ed06046de073d3aaf45664e804877c1a38efff
dinhthuyvy1505-eng <dinhthuyvy1505@gmail.com>
2025-11-13 10:39:02 +0700

    vy/feate-cat-payment

--- booksale/booksale_app/views/__init__.py
+++ booksale/booksale_app/views/__init__.py
@@ -4,5 +4,8 @@ from .admin_view.sanpham import product_list
 from .admin_view.chitietsanpham import product_detail
 from .authen_view import register_view, login_view, logout_view
 from .admin_view.order_view import order_list, order_detail, order_cancel_status, order_confirm_status
+from .user_view.giohang import giohang
+from .user_view.thanhtoan import thanhtoan
+from .user_view.donhang import donhang
 # Import các hàm view từ product.py
 # from .product import index as product_index, detail as product_detail
